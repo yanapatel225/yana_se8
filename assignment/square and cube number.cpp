a4f5d8df197bcd1e0aee1aa38f7f49efa1353891
yana patel <yanapatel1302@gmail.com>
2023-11-02 13:00:05 +0530

    Assignments

---
+++ assignment/square and cube number.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int no;
+	printf("enter your number : ");
+	scanf("%d", &no);
+	
+	printf("square of %d is  %d\n", no, (no * no));
+	printf("cube of %d is %d\n", no, (no * no * no));
+}
